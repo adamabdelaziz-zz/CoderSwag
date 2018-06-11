c2e42e0b93ba8d27d3dc5ac562827e55d7ab08a7
Adam Abdelaziz <adamabdelaziz@users.noreply.github.com>
2018-06-10 21:00:28 -0400

    Category List XML

--- app/src/main/java/com/androidadam/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/androidadam/coderswag/controller/MainActivity.kt
@@ -2,12 +2,26 @@ package com.androidadam.coderswag.controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.androidadam.coderswag.R
+import com.androidadam.coderswag.model.Category
+import com.androidadam.coderswag.services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                DataService.categories)
+
+        categoryListView.adapter = adapter
+
     }
 }
