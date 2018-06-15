4e8286923be35f9ad41612920dc55bc223a9fdb6
Adam Abdelaziz <adamabdelaziz@users.noreply.github.com>
2018-06-15 17:41:42 -0400

    ListView

--- app/src/main/java/com/androidadam/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/androidadam/coderswag/controller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.androidadam.coderswag.controller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.androidadam.coderswag.R
 import com.androidadam.coderswag.adapters.CategoryAdapter
 import com.androidadam.coderswag.model.Category
@@ -25,5 +26,11 @@ class MainActivity : AppCompatActivity() {
 
         categoryListView.adapter = adapter
 
+        /*categoryListView.setOnItemClickListener { adapterView, view, i, l ->
+            val category = DataService.categories[i]
+            Toast.makeText(this, "You clicked  on the ${category.title} cell", Toast.LENGTH_SHORT).show()
+        }
+        On click listeners for list view, wont work in recycleView.
+        */
     }
 }
