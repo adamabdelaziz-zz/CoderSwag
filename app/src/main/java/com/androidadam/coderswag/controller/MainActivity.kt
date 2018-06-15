e975c7de95d39aab0b6f3c7a4c17ce98d2801a8b
Adam Abdelaziz <adamabdelaziz@users.noreply.github.com>
2018-06-15 18:13:23 -0400

    RecyclerView First
    Refactored for recycleView

--- app/src/main/java/com/androidadam/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/androidadam/coderswag/controller/MainActivity.kt
@@ -2,10 +2,12 @@ package com.androidadam.coderswag.controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import com.androidadam.coderswag.R
 import com.androidadam.coderswag.adapters.CategoryAdapter
+import com.androidadam.coderswag.adapters.CategoryRecycleAdapter
 import com.androidadam.coderswag.model.Category
 import com.androidadam.coderswag.services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,18 +15,19 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter: CategoryAdapter
+    lateinit var adapter: CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryAdapter(
-                this,
-                DataService.categories
-        )
-
+        adapter = CategoryRecycleAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
+
+        val layoutManager = LinearLayoutManager(this)
+        categoryListView.layoutManager = layoutManager
+        categoryListView.setHasFixedSize(true)
+
 
         /*categoryListView.setOnItemClickListener { adapterView, view, i, l ->
             val category = DataService.categories[i]
